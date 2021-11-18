03d520fbafe17cce6f2cc11a0b52716daf8cbfd1
Snow_flakes404 <74827801+bestgamer2050@users.noreply.github.com>
2021-11-19 02:18:45 +0700

    Update UbuntuOnIntelAtom.sh

--- UbuntuOnIntelAtom.sh
+++ UbuntuOnIntelAtom.sh
@@ -1,9 +1,9 @@
 wget https://download1523.mediafire.com/p0adg3e8yhog/pbxn7h283n7i54p/isorespin.sh
 clear
-wget https://releases.ubuntu.com/21.10/ubuntu-21.10-desktop-amd64.iso
+wget https://mirrors.layeronline.com/linuxmint/stable/20.2/linuxmint-20.2-cinnamon-64bit.iso
 clear
 sudo chmod 755 isorespin.sh
-./isorespin.sh -i ubuntu-21.10-desktop-amd64.iso --atom
+./isorespin.sh -i linuxmint-20.2-cinnamon-64bit.iso --atom
 clear
 echo success
 exit
