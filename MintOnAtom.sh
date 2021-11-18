cde07755cf1bd92d1629516d467d763304c08b1d
Snow_flakes404 <74827801+bestgamer2050@users.noreply.github.com>
2021-11-19 02:26:08 +0700

    Update MintOnAtom.sh

--- MintOnAtom.sh
+++ MintOnAtom.sh
@@ -1,3 +1,7 @@
+echo -------------------------------------
+echo ----------Mint-on-Atom---------------
+echo -------------------------------------
+
 wget https://download1523.mediafire.com/p0adg3e8yhog/pbxn7h283n7i54p/isorespin.sh
 clear
 wget https://mirrors.layeronline.com/linuxmint/stable/20.2/linuxmint-20.2-cinnamon-64bit.iso
