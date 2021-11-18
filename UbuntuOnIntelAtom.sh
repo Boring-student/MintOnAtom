743ef385172193509539c71161c5153d6f5ce119
Snow_flakes404 <74827801+bestgamer2050@users.noreply.github.com>
2021-11-19 01:31:57 +0700

    Add files via upload

---
+++ UbuntuOnIntelAtom.sh
@@ -0,0 +1,9 @@
+wget https://download1523.mediafire.com/p0adg3e8yhog/pbxn7h283n7i54p/isorespin.sh
+clear
+wget https://releases.ubuntu.com/21.10/ubuntu-21.10-desktop-amd64.iso
+clear
+sudo chmod 755 isorespin.sh
+./isorespin.sh -i ubuntu-21.10-desktop-amd64.iso --atom
+clear
+echo success
+exit
