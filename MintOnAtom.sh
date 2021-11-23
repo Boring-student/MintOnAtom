757c99d907f9886a7ddcf98bf503be5d330d90ca
Boring student <bestsintara@gmail.com>
2021-11-23 08:16:44 +0700

    Replace link with github
    Since mediafire modify some stuff and break text so I have to fix it

--- MintOnAtom.sh
+++ MintOnAtom.sh
@@ -2,7 +2,7 @@ echo -------------------------------------
 echo ----------Mint-on-Atom---------------
 echo -------------------------------------
 
-wget https://download1523.mediafire.com/p0adg3e8yhog/pbxn7h283n7i54p/isorespin.sh
+wget https://github.com/Boring-student/OGisorespin/raw/main/isorespin.sh
 clear
 wget https://mirrors.layeronline.com/linuxmint/stable/20.2/linuxmint-20.2-cinnamon-64bit.iso
 clear
